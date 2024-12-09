c608eb8080b14abd468d8c241c5af7e1e446729a
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-12-09 21:04:44 +0545

    added navbar, images, text and some styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,5 @@
+import { Input } from "./ui/input";
+
 const Navbar = () => {
   return (
     <div className="flex flex-col">
@@ -9,13 +11,26 @@ const Navbar = () => {
           <div>Login</div>
         </div>
       </div>
-      <div className="flex justify-center flex-col text-center ">
-        <div className="text-3xl font-bold">
-          Find a <span className="text-purple-600">GO-TO</span> parking space
+      <div className="flex justify-between pl-5">
+        <div className="flex justify-center flex-col text-center gap-5">
+          <div className="text-3xl font-bold">
+            Find a <span className="text-purple-600">GO-TO</span> parking space
+          </div>
+          <div className="text-gray-500">
+            Discover and reserve secure, convenient parking near you in seconds.
+            Stress-free parking, always!
+          </div>
+          <div>
+            <Input
+              type="email"
+              placeholder="Search a place"
+              className="w-[70%]"
+            />
+            <Icon icon="line-md:search" width="24" height="24" />
+          </div>
         </div>
         <div>
-          Discover and reserve secure, convenient parking near you in seconds.
-          Stress-free parking, always!
+          <img src="./park.png" alt="parking" />
         </div>
       </div>
     </div>
