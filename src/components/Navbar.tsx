10d9c06f378dc1703d90597f9354f3be1abc2854
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-12-10 14:53:30 +0545

    features added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -31,8 +31,8 @@ const Navbar = () => {
         <div className="flex justify-between pl-5">
           <div className="flex justify-center flex-col text-center gap-5">
             <div className="text-3xl font-bold">
-              Find a <span className="text-purple-600">GO-TO</span> parking
-              space
+              Find and Reserve <span className="text-purple-600">Parking</span>{" "}
+              Instantly!
             </div>
             <div className="text-gray-500">
               Discover and reserve secure, convenient parking near you in
@@ -50,15 +50,15 @@ const Navbar = () => {
         </div>
         <Seperator></Seperator>
       </div>
-      <div className="flex flex-col gap-10 items-center pt-20 w-full ">
-        <div className="text-2xl font-bold">Have an unused space?</div>
+      <div className="flex flex-col gap-14 items-center pt-20 w-full ">
+        <div className="text-5xl font-bold">Have an unused space?</div>
         <div className="text-gray-500 text-xl font-bold">
           Become a host in few steps
         </div>
         <div className="flex justify-between gap-44">
           <div>
             <img src="/first.png" alt="" className="h-40" />
-            <h2 className="flex justify-center">list your space</h2>
+            <h2 className="flex justify-center">List your space</h2>
           </div>
           <div>
             <img src="/second.png" alt="" className="h-40" />
@@ -66,10 +66,79 @@ const Navbar = () => {
           </div>
           <div>
             <img src="/earn.png" alt="" className="h-40" />
-            <h2 className="flex justify-center">start learning</h2>
+            <h2 className="flex justify-center">Start learning</h2>
           </div>
         </div>
-        <Button className="bg-purple-500 px-10">Become a host</Button>
+        <Button className="bg-purple-500 px-10 hover:bg-purple-600">
+          Become a host
+        </Button>
+      </div>
+      <Seperator></Seperator>
+      <div className=" flex justify-center flex-col pt-14 mb-14 ">
+        <div className="flex justify-center text-center text-3xl font-bold">
+          Features we <span className="text-purple-500"> Offer</span>
+        </div>
+        <div className="flex justify-between  ">
+          <div className="flex flex-col gap-4">
+            <div>
+              <div>Search by location</div>
+              <div>
+                Quickly find parking spaces near landmarks, addresses, or
+                cities.
+              </div>
+            </div>
+            <div>
+              <div>Real-time availability</div>
+              <div>
+                View up-to-date information on available spots for immediate or
+                future bookings.
+              </div>
+            </div>
+            <div>
+              <div>Flexible booking and reservation</div>
+              <div>
+                Reserve parking for your desired duration—hourly, daily, or
+                monthly.
+              </div>
+            </div>
+            <div>
+              <div>Secure Online Payments</div>
+              <div>
+                Pay seamlessly with multiple payment options, ensuring a safe
+                transaction.
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-4">
+            <div>
+              <div>Navigation Assistance</div>
+              <div>
+                Get step-by-step directions to your reserved parking spot via
+                integrated maps.
+              </div>
+            </div>
+            <div>
+              <div>Filter and Sort</div>
+              <div>
+                Refine searches by price, amenities (e.g., EV charging), or type
+                (covered/uncovered).
+              </div>
+            </div>
+            <div>
+              <div>Parking History</div>
+              <div>
+                Keep track of your past bookings and receipts for convenience.
+              </div>
+            </div>
+            <div>
+              <div>Customer Reviews</div>
+              <div>
+                Read feedback from other parkers to choose the best parking
+                space for your needs.
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
