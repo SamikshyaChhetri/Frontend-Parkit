46625a53aef72bf75e9c5b1f9f10f0dedbf8dbaf
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-12-10 22:51:26 +0545

    added a new component

---
+++ src/components/Features.tsx
@@ -0,0 +1,24 @@
+import { FC, ReactNode } from "react";
+
+const Features: FC<{
+  title: string;
+  description: string;
+  icon: ReactNode;
+  color: string;
+}> = ({ title, description, icon, color }) => {
+  return (
+    <div className="flex gap-3 mt-10 ">
+      <div
+        className={`flex justify-center items-center rounded-full w-14 ${color} `}
+      >
+        {icon}
+      </div>
+      <div className="flex flex-col ">
+        <div className="text-lg font-bold">{title}</div>
+        <div>{description}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Features;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Icon } from "@iconify/react/dist/iconify.js";
+import Features from "./Features";
 import Searchbox from "./Searchbox";
 import Seperator from "./Seperator";
 
@@ -74,19 +75,27 @@ const Navbar = () => {
         </Button>
       </div>
       <Seperator></Seperator>
-      <div className=" flex justify-center flex-col pt-14 ">
+      {/* <div className=" flex justify-center flex-col pt-14 text-justify  ">
         <div className="flex justify-center text-center text-3xl font-bold pb-14">
           Features we <span className="text-purple-500"> Offer</span>
         </div>
         <div className="flex justify-evenly">
           <div className="flex flex-col gap-4">
-            <div>
-              <div className="font-bold text-lg">Search by location</div>
+            <div className="flex gap-2 items-center">
+              <Icon
+                icon="tdesign:location"
+                className="w-14 h-14 text-white bg-green-400 p-4 rounded-full"
+              />
+
               <div>
-                Quickly find parking spaces near landmarks, addresses, or
-                cities.
+                <div className="font-bold text-lg">Search by location</div>
+                <div>
+                  Quickly find parking spaces near landmarks, addresses, or
+                  cities.
+                </div>
               </div>
             </div>
+
             <div>
               <div className="font-bold text-lg">Real-time availability</div>
               <div>
@@ -199,9 +208,45 @@ const Navbar = () => {
             </div>
           </div>
         </div>
+      </div> */}
+      <div className="flex justify-center text-2xl font-bold pt-10">
+        Features we <span className="text-purple-500"> Offer</span>
       </div>
+      <Features
+        title="Search by Location"
+        description="Quickly find parking spaces near landmarks, addresses, or cities"
+        icon={<Icon icon="tdesign:location" width="24" height="24" />}
+        color="bg-red-500"
+      ></Features>
+      <Features
+        title="Real-Time Availability"
+        description="View up-to-date information on available spots for immediate or future bookings."
+        icon={<Icon icon="bx:time-five" width="24" height="24" />}
+      ></Features>
+      <Features
+        title="Secure Online Payments"
+        description="Pay seamlessly with multiple payment options, ensuring a safe transaction."
+        icon={<Icon icon="ic:outline-payment" width="24" height="24" />}
+      ></Features>
+      <Features
+        title="Navigation Assistance"
+        description="Get step-by-step directions to your reserved parking spot via integrated maps"
+        icon={<Icon icon="bx:navigation" width="24" height="24" />}
+      ></Features>
+      <Features
+        title="Parking History
+"
+        description="Keep track of your past bookings and receipts for convenience.
+
+"
+        icon={<Icon icon="ic:twotone-manage-history" width="24" height="24" />}
+      ></Features>
     </div>
   );
 };
 
+// aba ali styling gara
+
 export default Navbar;
+// k garne kkkkk  tailwind class pass garne k, aba red colro chaiyexa vane bg-red-500 pass garne, ani teslai uuta lera tyo div ma haldine, bujhenau?
+// mathi hera ta
