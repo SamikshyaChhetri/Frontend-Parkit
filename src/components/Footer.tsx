b36214db072d8aa57f53bdaa70487e756e834a7e
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-12-11 21:13:24 +0545

    Footer component added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,8 @@
+import { Icon } from "@iconify/react/dist/iconify.js";
+
 const Footer = () => {
   return (
-    <div className="bg-gray-800 text-white p-10">
+    <div className="bg-gray-800 text-white pt-10 px-4 pb-5 flex flex-col gap-10">
       <div className="flex justify-between">
         <div className="flex flex-col w-[25%] gap-2">
           <div className="font-bold text-xl">Parking</div>
@@ -36,6 +38,20 @@ const Footer = () => {
             Founded in 2025, Our platform offers real-time availability, secure
             booking, flexible pricing, and options tailored to your needs.
           </div>
+        </div>
+      </div>
+      <div className="flex justify-between ">
+        <div className="pl-10">Parkify,inc</div>
+        <div className="flex justify-between gap-10">
+          <div>Privacy</div>
+          <div>Terms</div>
+          <div>Contact</div>
+        </div>
+        <div className="flex justify-between gap-8 pr-10 ">
+          <Icon icon="skill-icons:instagram" width="24" height="24" />
+          <Icon icon="logos:facebook" width="24" height="24" />
+          <Icon icon="logos:tiktok-icon" width="24" height="24" />
+          <Icon icon="mingcute:threads-fill" width="24" height="24" />
         </div>
       </div>
     </div>
