75cc0dfe616ad05165221dab702b3180e365a51f
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-12-11 19:59:57 +0545

    marquee library implemented

--- src/components/MarqueeDemo.tsx
+++ src/components/MarqueeDemo.tsx
@@ -17,40 +17,48 @@ const reviews = [
 
 export function MarqueeDemo() {
   return (
-    <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
-      <Marquee pauseOnHover className="[--duration:20s]">
-        <div>
-          <img
-            src="./van.jpeg"
-            alt=""
-            className="h-80 w-64 rounded-3xl bg-pu"
-          />
-          <div className="flex justify-center items-center ">
-            <div className="text-center font-bold bg-gray-500 rounded-lg w-[100%]  text-white p-2 mt-2 ">
-              Van parking
+    <div className="flex flex-col gap-10">
+      <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
+        <Marquee pauseOnHover className="[--duration:20s]">
+          <div>
+            <img
+              src="./van.jpeg"
+              alt=""
+              className="h-80 w-64 rounded-3xl bg-pu"
+            />
+            <div className="flex justify-center items-center ">
+              <div className="text-center font-bold bg-gray-500 rounded-lg w-[100%]  text-white p-2 mt-2 ">
+                Van parking
+              </div>
             </div>
           </div>
-        </div>
 
-        <div>
-          <img src="./car1.avif" alt="" className="h-80 w-64 rounded-3xl" />
-          <div className="flex justify-center items-center ">
-            <div className="text-center font-bold bg-gray-500 rounded-lg w-[100%] text-white p-2 mt-2  ">
-              Car parking
+          <div>
+            <img src="./car1.avif" alt="" className="h-80 w-64 rounded-3xl" />
+            <div className="flex justify-center items-center ">
+              <div className="text-center font-bold bg-gray-500 rounded-lg w-[100%] text-white p-2 mt-2  ">
+                Car parking
+              </div>
             </div>
           </div>
-        </div>
-        <div>
-          <img src="./bike.jpg" alt="" className="h-80 w-64 rounded-3xl" />
-          <div className="flex justify-center items-center ">
-            <div className="text-center font-bold bg-gray-500 rounded-lg w-[100%] text-white p-2 mt-2 ">
-              Two wheeler parking
+          <div>
+            <img src="./bike.jpg" alt="" className="h-80 w-64 rounded-3xl" />
+            <div className="flex justify-center items-center ">
+              <div className="text-center font-bold bg-gray-500 rounded-lg w-[100%] text-white p-2 mt-2 ">
+                Two wheeler parking
+              </div>
             </div>
           </div>
+        </Marquee>
+        <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-white dark:from-background"></div>
+        <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-white dark:from-background"></div>
+      </div>
+      <div className="flex flex-col text-center gap-3">
+        <div className="font-bold text-2xl ">Options for your every need</div>
+        <div>
+          Safe, Closer and Convenient parking space in palm of your hands{" "}
         </div>
-      </Marquee>
-      <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-white dark:from-background"></div>
-      <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-white dark:from-background"></div>
+      </div>
     </div>
   );
 }
--- src/components/Seperator.tsx
+++ src/components/Seperator.tsx
@@ -1,7 +1,7 @@
 const Seperator = () => {
   return (
     <div>
-      <div className="border-[1px] border-gray-600 mt-[100px] mx-[100px]"></div>
+      <div className="border-[1px] border-gray-600 mt-[100px] mx-[100px] mb-[100px]"></div>
     </div>
   );
 };
