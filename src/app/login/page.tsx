3be01d8f62127985c0b031b67cba840d91177e59
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-12-13 18:47:47 +0545

    Implemented form validation

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -12,34 +12,37 @@ import { z } from "zod";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Icon } from "@iconify/react/dist/iconify.js";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+
 const createSchema = z.object({
-  email: z.string().email(" Invalid email id"),
-  password: z.string().min(6).max(20).password("Invalid password"),
+  email: z.string().email("Invalid email ID"),
+  password: z
+    .string()
+    .min(6, "Password must be at least 6 characters long")
+    .max(20, "Password must not exceed 20 characters"),
 });
 
 const Page = () => {
   const [isOpen, setOpen] = useState(true);
 
-  const Form = () => {
-    const newForm = useForm({
-      defaultValues: {
-        email: "",
-        password: "",
-      },
-    });
+  const form = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    resolver: zodResolver(createSchema),
+  });
+  const onsubmit = (data: { email: string; password: string }) => {
+    console.log(data);
   };
   return (
     <div className="bg-gradient-to-r from-purple-400 to-white">
       <div className="flex justify-center h-screen items-center">
-        <Card className="w-[450px] flex flex-col gap-8 bg-gradient-to-r from-purple-200 to-white">
-          <form
-            onSubmit={() => {
-              console.log(newForm);
-            }}
-          >
+        <Card className="w-[450px] flex flex-col bg-gradient-to-r from-purple-200 to-white">
+          <form onSubmit={form.handleSubmit(onsubmit)}>
             <CardHeader>
               <CardTitle className="flex justify-center text-2xl font-bold text-purple-600">
                 Login Form
@@ -49,7 +52,7 @@ const Page = () => {
               </CardDescription>
             </CardHeader>
             <CardContent>
-              <form className="flex flex-col gap-6">
+              <div className="flex flex-col gap-6">
                 <div className="flex flex-col gap-2">
                   <Label htmlFor="email" className="flex gap-1 items-center">
                     Email
@@ -57,8 +60,11 @@ const Page = () => {
                   </Label>
                   <Input
                     placeholder="Enter your email"
-                    {...newForm.register("email")}
+                    {...form.register("email")}
                   />
+                  <label className="text-red-500 text-sm">
+                    {form.formState.errors.email?.message}
+                  </label>
                 </div>
                 <div className="flex flex-col gap-2 relative">
                   <Label htmlFor="password" className="flex gap-1 items-center">
@@ -73,8 +79,11 @@ const Page = () => {
                   <Input
                     placeholder="Enter your password"
                     type={isOpen ? "text" : "password"}
-                    {...newForm.register("password")}
+                    {...form.register("password")}
                   />
+                  <label className="text-red-500 text-sm">
+                    {form.formState.errors.password?.message}
+                  </label>
                   <Icon
                     icon={
                       isOpen ? "heroicons-solid:eye" : "heroicons-solid:eye-off"
@@ -96,14 +105,13 @@ const Page = () => {
                 </div>
 
                 <Button>Submit</Button>
-
-                <div className="flex justify-center items-center">
-                  Don't have an account?
-                  <Button variant={"link"}>Register</Button>
-                </div>
-              </form>
+              </div>
             </CardContent>
           </form>
+          <div className="flex justify-center items-center pb-5">
+            Don't have an account?
+            <Button variant={"link"}>Register</Button>
+          </div>
         </Card>
       </div>
     </div>
