20725b51a50d82f2044545cf7dd6303bdd58758d
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-12-25 15:23:37 +0545

    Added icon and sidebar elements

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,6 @@
+import { Icon } from "@iconify/react/dist/iconify.js";
 import Search from "./Search";
 import { Button } from "./ui/button";
-import { Input } from "./ui/input";
-import { Label } from "./ui/label";
 import {
   Sheet,
   SheetClose,
@@ -32,7 +31,7 @@ const Header = () => {
                     className="bg-gray-400 w-12 h-12 border border-black rounded-full "
                   ></Button>
                 </SheetTrigger>
-                <SheetContent>
+                <SheetContent className="flex flex-col gap-10">
                   <SheetHeader className="flex gap-5 flex-row">
                     <div className="bg-gray-400 w-14 border border-black rounded-full "></div>
                     <div>
@@ -42,26 +41,36 @@ const Header = () => {
                       </SheetDescription>
                     </div>
                   </SheetHeader>
-                  <div className="grid gap-4 py-4">
-                    <div className="grid grid-cols-4 items-center gap-4">
-                      <Label htmlFor="name" className="text-right">
-                        Name
-                      </Label>
-                      <Input
-                        id="name"
-                        value="Pedro Duarte"
-                        className="col-span-3"
-                      />
+                  <div className="flex flex-col gap-6">
+                    <div className="flex gap-2">
+                      <Icon icon="clarity:home-solid" width="24" height="24" />
+                      <div className=" flex justify-center ">Home</div>
                     </div>
-                    <div className="grid grid-cols-4 items-center gap-4">
-                      <Label htmlFor="username" className="text-right">
-                        Username
-                      </Label>
-                      <Input
-                        id="username"
-                        value="@peduarte"
-                        className="col-span-3"
+                    <div className="flex gap-2">
+                      <Icon
+                        icon="tdesign:location-filled"
+                        width="24"
+                        height="24"
                       />
+                      <div>Places</div>
+                    </div>
+                    <div className="flex gap-2">
+                      <Icon
+                        icon="mdi:file-document-box-tick-outline"
+                        width="24"
+                        height="24"
+                        className="inline"
+                      />
+                      <div>Reservation</div>
+                    </div>
+                    <div>
+                      <Icon
+                        icon="ion:list-sharp"
+                        width="24"
+                        height="24"
+                        className="inline"
+                      />
+                      Your Listings
                     </div>
                   </div>
                   <SheetFooter>
