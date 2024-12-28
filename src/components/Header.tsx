feecd3f0ddfb956256b1a48cdeb0fa0962536b62
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-12-28 15:42:28 +0545

    implemented dynamic name and email

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { Icon } from "@iconify/react/dist/iconify.js";
+import { FC } from "react";
 import Search from "./Search";
 import { Button } from "./ui/button";
 import {
@@ -18,7 +19,10 @@ const sideBarItems = [
   { menu: "Your Listings", icon: "ion:list-sharp" },
 ];
 
-const Header = () => {
+const Header: FC<{
+  name: string;
+  email: string;
+}> = ({ name, email }) => {
   return (
     <div className="text-white bg-gray-800 h-screen">
       <div className="flex flex-col gap-6">
@@ -37,27 +41,28 @@ const Header = () => {
                     className="bg-gray-400 w-12 h-12 border border-black rounded-full"
                   ></Button>
                 </SheetTrigger>
-                <SheetContent className="flex flex-col  rounded-lg m-2 bg-gray-800 text-white h-[95%]">
-                  <div className="flex flex-col gap-10">
-                    <SheetHeader className="flex gap-3 flex-row">
-                      <div className="bg-gray-400 w-14 border border-black rounded-full"></div>
-                      <div>
-                        <SheetTitle className="text-white">
-                          Samikshya Baniya
-                        </SheetTitle>
-                        <SheetDescription>
-                          samikshyabchhetri@gmail.com
-                        </SheetDescription>
-                      </div>
-                    </SheetHeader>
-                  </div>
-                  <div className="flex flex-col gap-6 mt-6">
-                    {sideBarItems.map((item) => (
-                      <div key={item.menu} className="flex gap-2">
-                        <Icon icon={item.icon} width="24" height="24" />
-                        <div>{item.menu}</div>
-                      </div>
-                    ))}
+                <SheetContent className="flex flex-col justify-between  rounded-lg m-2 bg-gray-800 text-white h-[90%]">
+                  <div className="flex flex-col gap-8">
+                    <div className="flex flex-col gap-10">
+                      <SheetHeader className="flex gap-3 flex-row">
+                        <div className="bg-gray-400 w-14 border border-black rounded-full"></div>
+                        <div>
+                          <SheetTitle className="text-white">{name}</SheetTitle>
+                          <SheetDescription>{email}</SheetDescription>
+                        </div>
+                      </SheetHeader>
+                    </div>
+                    <div className="flex flex-col   ">
+                      {sideBarItems.map((item) => (
+                        <div
+                          key={item.menu}
+                          className="flex gap-2 p-3 hover:bg-gray-500 rounded-lg"
+                        >
+                          <Icon icon={item.icon} width="24" height="24" />
+                          <div>{item.menu}</div>
+                        </div>
+                      ))}
+                    </div>
                   </div>
 
                   <SheetFooter>
