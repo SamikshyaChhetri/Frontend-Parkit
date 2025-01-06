157d18f06e2ea5df96617bd27733161ff395fa2d
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2025-01-06 23:14:08 +0545

    function added to capilize first letter

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,11 +5,9 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-
-export const a = 10;  // this is normal export 
-
-const b = 20;
-export default b;   // this is default export (1 liner mildaina)
-
-// aba difference import garda dekhinxa
-// lets go to page.tsx   uuta aau na
+export const capitalize=(input:string)=>{
+  const splittedInput=input.split("");
+  const capitalizedLetter=splittedInput[0].toUpperCase();
+  splittedInput[0]=capitalizedLetter;
+  return splittedInput.join("");
+}
