88edc6b5bb5da06d4b4087bc6ab0b5d8491389bb
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2025-01-06 17:36:49 +0545

    card designed

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -14,22 +14,22 @@ const Display: FC<{
   }[];
 }> = ({ listingQueryData }) => {
   return (
-    <div className="flex justify-center mx-12">
+    <div className="flex justify-between ml-10">
       <div className="flex flex-wrap flex-1 gap-4 pt-5">
         {listingQueryData.map((item, index) => {
           return (
-            <div key={index} className=" flex flex-col gap-5 ">
+            <div key={index} className=" flex flex-col gap-3 ">
               <Card>
                 {/* <div>{item.description}</div> */}
-                <div className="h-72 w-80">
+                <div className="h-52 w-64">
                   <img src={item.image} alt="" />
                 </div>
               </Card>
-              <CardContent className="text-white">
+              <CardContent className="text-white flex flex-col justify-start ">
                 <CardTitle>
                   {item.city},{item.country}
                 </CardTitle>
-                <CardDescription>{item.price}</CardDescription>
+                <CardDescription>Rs.{item.price} per hour</CardDescription>
               </CardContent>
             </div>
           );
