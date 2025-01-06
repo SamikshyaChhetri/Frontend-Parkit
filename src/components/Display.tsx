9249e947c1af770801526369246181c506e0fea3
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2025-01-06 17:23:38 +0545

    listings displayed to dashboad

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,16 +1,42 @@
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
-
-const Display = () => {
-  const userQuery = useQuery({
-    queryKey: ["allListings"],
-    queryFn: async () => {
-      const response = await axios.get("http://localhost:3333/listing");
-      console.log(response.data);
-      return response.data;
-    },
-  });
-  return <div>Display</div>;
+import { FC } from "react";
+import { Card, CardContent, CardDescription, CardTitle } from "./ui/card";
+const Display: FC<{
+  listingQueryData: {
+    description: string;
+    type: string;
+    price: string;
+    numberOfVehicle: string;
+    city: string;
+    street: string;
+    country: string;
+    zipcode: string;
+    image: string;
+  }[];
+}> = ({ listingQueryData }) => {
+  return (
+    <div className="flex justify-center mx-12">
+      <div className="flex flex-wrap flex-1 gap-4 pt-5">
+        {listingQueryData.map((item, index) => {
+          return (
+            <div key={index} className=" flex flex-col gap-5 ">
+              <Card>
+                {/* <div>{item.description}</div> */}
+                <div className="h-72 w-80">
+                  <img src={item.image} alt="" />
+                </div>
+              </Card>
+              <CardContent className="text-white">
+                <CardTitle>
+                  {item.city},{item.country}
+                </CardTitle>
+                <CardDescription>{item.price}</CardDescription>
+              </CardContent>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Display;
