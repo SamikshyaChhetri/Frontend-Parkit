779ec57bcba8a8ff9ff2b9dd402abdaa5334e600
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2025-01-08 16:57:30 +0545

    fetched single listing

--- src/app/p/[userId]/listings/[listingsId]/page.tsx
+++ src/app/p/[userId]/listings/[listingsId]/page.tsx
@@ -1,14 +1,38 @@
+"use client";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import { FC, Usable, use } from "react";
 
-const page: FC<{ params: Usable<{ listingsId: string; userId: string }> }> = ({
-  params,
-}) => {
+const page: FC<{
+  params: Usable<{
+    listingsId: string;
+    userId: string;
+    city: string;
+    street: String;
+    country: string;
+    zipcode: string;
+    type: string;
+    description: string;
+    noOfVehicle: string;
+    photo: string;
+  }>;
+}> = ({ params }) => {
   const rparams = use(params);
-  return (
-    <div>
-      {rparams.listingsId} {rparams.userId}
-    </div>
-  );
-};
+  const listingsQuery = useQuery({
+    queryKey: ["singleUserQuery"],
+    queryFn: async () => {
+      const response = await axios.get(
+        `http://localhost:3333/listing/${rparams.listingsId}`
+      );
+      return response.data;
+    },
+  });
 
+  if (listingsQuery.isLoading || listingsQuery.isFetching) {
+    return "Loading...";
+  }
+
+  return <div>{listingsQuery.data.data.price}</div>;
+};
 export default page;
+// viber for explanantion okk
