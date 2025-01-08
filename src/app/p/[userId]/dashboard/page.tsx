f801e646ffb07e5474b9b56e1c6ff6f5fa7e6f26
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2025-01-08 22:09:46 +0545

    useQuery implemented

--- src/app/p/[userId]/dashboard/page.tsx
+++ src/app/p/[userId]/dashboard/page.tsx
@@ -1,23 +1,11 @@
 "use client";
 import Display from "@/components/Display";
-import Header from "@/components/Header";
 import Search from "@/components/Search";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import React, { FC, Usable } from "react";
 const page: FC<{ params: Usable<{ userId: string }> }> = ({ params }) => {
   const rparams = React.use(params);
-  const userQuery = useQuery({
-    queryKey: ["singleUserQuery"],
-    queryFn: async () => {
-      const response = await axios.get(
-        `http://localhost:3333/users/getSingleuser/${rparams.userId}`
-      );
-      return response.data;
-    },
-  });
-  // const resolvedParams = React.use(params);
-
   const listingQuery = useQuery({
     queryKey: ["allListings"],
     queryFn: async () => {
@@ -26,16 +14,9 @@ const page: FC<{ params: Usable<{ userId: string }> }> = ({ params }) => {
       return response.data;
     },
   });
-  if (userQuery.isLoading) {
-    return <div>Loading...</div>;
-  }
+
   return (
     <div className="bg-gray-800 h-screen flex flex-col gap-8">
-      <Header
-        name={userQuery.data.name}
-        email={userQuery.data.email}
-        avatar={userQuery.data.avatar}
-      ></Header>
       <Search></Search>
 
       {listingQuery.isSuccess ? (
