bb7ccfaff51b04a211b3933d6c0092323360b109
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2025-01-08 22:09:17 +0545

    layout added

--- src/app/p/[userId]/layout.tsx
+++ src/app/p/[userId]/layout.tsx
@@ -1,5 +1,18 @@
-const layout = () => {
-  return <div>layout</div>;
+import Header from "@/components/Header";
+import React, { FC, ReactNode, Usable } from "react";
+
+const layout: FC<{
+  children: ReactNode;
+  params: Usable<{ userId: string }>;
+}> = ({ children, params }) => {
+  const rparams = React.use(params);
+
+  return (
+    <div>
+      <Header userId={rparams.userId}></Header>
+      {children}
+    </div>
+  );
 };
 
 export default layout;
