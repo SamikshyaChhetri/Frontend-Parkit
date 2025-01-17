8b9cb19f0e7c115d7af02ce504d8b19c2d0c9a49
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2025-01-17 22:54:30 +0545

    refactor: replace card component with ListingCard for better structure

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,7 +1,6 @@
-import { capitalize } from "@/lib/utils";
 import { useRouter } from "next/navigation";
 import { FC } from "react";
-import { Card, CardContent, CardDescription } from "./ui/card";
+import ListingCard from "./ListingCard";
 const Display: FC<{
   userId: string;
   listingQueryData: {
@@ -20,7 +19,7 @@ const Display: FC<{
   const router = useRouter();
   return (
     <div className="sm:px-5 bg-gray-800 sm:h-screen sm:pb-5  ">
-      <div className="grid sm:grid-cols-3  sm:flex-wrap sm:flex-1 w-full gap-4 lg:grid-cols-5  ">
+      <div className="grid sm:grid-cols-3  flex-wrap sm:flex-1 w-full gap-4 lg:grid-cols-5  ">
         {listingQueryData.map((item, index) => {
           return (
             <div
@@ -28,25 +27,13 @@ const Display: FC<{
                 router.push(`/p/${userId}/listings/${item.id}`);
               }}
               key={index}
-              className=" flex flex-col sm:gap-3 sm:pt-10 mx-5 md:mx-0"
             >
-              <Card className="bg-transparent cursor-pointer hover:shadow-md hover:scale-[1.07] ease-in-out transition-all border-none">
-                <CardContent className="text-white flex flex-col h-fit p-0 ">
-                  <div>
-                    <img
-                      src={item.photo}
-                      className="h-52 sm:w-full rounded-xl w-full"
-                      alt=""
-                    />
-                  </div>
-                  <div className="p-2">
-                    <div className="text-white">
-                      {capitalize(item.city)},{capitalize(item.country)}
-                    </div>
-                    <CardDescription>Rs.{item.price} per hour</CardDescription>
-                  </div>
-                </CardContent>
-              </Card>
+              <ListingCard
+                photo={item.photo}
+                city={item.city}
+                country={item.country}
+                price={item.price}
+              ></ListingCard>
             </div>
           );
         })}
