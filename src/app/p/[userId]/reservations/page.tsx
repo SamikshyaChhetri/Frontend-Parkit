8d64374789def85187690dc9803b7bc0c4d220ef
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2025-01-18 23:51:12 +0545

    feat: integrate ListingCard component to display user listings in Reservations page

--- src/app/p/[userId]/reservations/page.tsx
+++ src/app/p/[userId]/reservations/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+import ListingCard from "@/components/ListingCard";
 import { useQuery } from "@tanstack/react-query";
 import axios, { AxiosResponse } from "axios";
 import React, { FC, Usable } from "react";
@@ -17,7 +19,24 @@ const page: FC<{ params: Usable<{ userId: string }> }> = ({ params }) => {
       return response.data;
     },
   });
-  return <div>page</div>;
+  return (
+    <div className="bg-gray-800 h-screen px-5">
+      <div className="flex justify-center text-2xl text-white py-7">
+        Your Listings
+      </div>
+      {userReservations.data?.data.map((item, index) => {
+        return (
+          <ListingCard
+            key={index}
+            city={item.city}
+            country={item.country}
+            photo={item.photo}
+            price={item.price}
+          ></ListingCard>
+        );
+      })}
+    </div>
+  );
 };
 
 export default page;
