96908cac815c5a46fd6a9ef2b4b37c73eaf5265f
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2025-01-19 21:52:39 +0545

    fix: update date format in ListingCard for better readability

--- src/components/ListingCard.tsx
+++ src/components/ListingCard.tsx
@@ -11,7 +11,7 @@ const ListingCard: FC<{
   date?: string;
 }> = ({ photo, city, country, price, type, date }) => {
   return (
-    <div className=" flex flex-col sm:gap-3 sm:pt-10 mx-5 md:mx-0 sm:w-full  md:w-fit  ">
+    <div>
       <Card className="bg-transparent cursor-pointer hover:shadow-md hover:scale-[1.07] ease-in-out transition-all border-none">
         <CardContent className="text-white flex flex-col h-fit p-0 ">
           <div>
@@ -30,7 +30,7 @@ const ListingCard: FC<{
             )}
             {type == "reservation" && (
               <CardDescription>
-                {moment(date).format("YYYY-MM-DD")}
+                {moment(date).format("DD MMM YYYY")}
               </CardDescription>
             )}
           </div>
