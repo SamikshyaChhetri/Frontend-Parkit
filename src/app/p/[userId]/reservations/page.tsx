72dc660c1324923f105c565e30c4c2ba0fb0ed12
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2025-01-19 21:30:21 +0545

    feat: update Reservations page to display user reservations with listing details

--- src/app/p/[userId]/reservations/page.tsx
+++ src/app/p/[userId]/reservations/page.tsx
@@ -11,7 +11,15 @@ const page: FC<{ params: Usable<{ userId: string }> }> = ({ params }) => {
     queryFn: async () => {
       const response: AxiosResponse<{
         message: string;
-        data: { city: string; country: string; photo: string; price: string }[];
+        data: {
+          listing: {
+            city: string;
+            country: string;
+            photo: string;
+            price: string;
+          };
+          date: string;
+        }[];
       }> = await axios.get(
         `http://localhost:3333/reserve/userReservations/${rparams.userId}`
       );
@@ -19,22 +27,26 @@ const page: FC<{ params: Usable<{ userId: string }> }> = ({ params }) => {
       return response.data;
     },
   });
+
   return (
     <div className="bg-gray-800 h-screen px-5">
       <div className="flex justify-center text-2xl text-white py-7">
-        Your Listings
+        Your Reservations
       </div>
-      {userReservations.data?.data.map((item, index) => {
-        return (
-          <ListingCard
-            key={index}
-            city={item.city}
-            country={item.country}
-            photo={item.photo}
-            price={item.price}
-          ></ListingCard>
-        );
-      })}
+      {userReservations.isSuccess &&
+        userReservations.data?.data.map((item, index) => {
+          return (
+            <ListingCard
+              key={index}
+              city={item.listing.city} // fix these
+              country={item.listing.country}
+              photo={item.listing.photo}
+              price={item.listing.price}
+              date={item.date}
+              type="reservation"
+            ></ListingCard>
+          );
+        })}
     </div>
   );
 };
