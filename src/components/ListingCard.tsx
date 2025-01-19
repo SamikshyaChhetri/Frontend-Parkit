b322286735ca6c0e135a94bbf12a6c224550bb23
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2025-01-19 21:34:07 +0545

    feat: add type and date props to ListingCard for conditional rendering

--- src/components/ListingCard.tsx
+++ src/components/ListingCard.tsx
@@ -1,13 +1,15 @@
 import { capitalize } from "@/lib/utils";
+import moment from "moment";
 import { FC } from "react";
 import { Card, CardContent, CardDescription } from "./ui/card";
-
 const ListingCard: FC<{
   photo: string;
   city: string;
   country: string;
   price: string;
-}> = ({ photo, city, country, price }) => {
+  type?: "listing" | "reservation";
+  date?: string;
+}> = ({ photo, city, country, price, type, date }) => {
   return (
     <div className=" flex flex-col sm:gap-3 sm:pt-10 mx-5 md:mx-0 sm:w-full  md:w-fit  ">
       <Card className="bg-transparent cursor-pointer hover:shadow-md hover:scale-[1.07] ease-in-out transition-all border-none">
@@ -23,7 +25,14 @@ const ListingCard: FC<{
             <div className="text-white">
               {capitalize(city)},{capitalize(country)}
             </div>
-            <CardDescription>Rs.{price} per hour</CardDescription>
+            {type == "listing" && (
+              <CardDescription>Rs.{price} per hour</CardDescription>
+            )}
+            {type == "reservation" && (
+              <CardDescription>
+                {moment(date).format("YYYY-MM-DD")}
+              </CardDescription>
+            )}
           </div>
         </CardContent>
       </Card>
