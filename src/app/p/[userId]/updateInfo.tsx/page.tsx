785964b2d9f7624fc2ac5cd0b16da875862b36fa
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2025-01-26 20:28:59 +0545

    add ManageDetails component to user updateInfo page

---
+++ src/app/p/[userId]/updateInfo.tsx/page.tsx
@@ -0,0 +1,12 @@
+import ManageDetails from "@/components/ManageDetails";
+
+const page = () => {
+  return (
+    <div>
+      <ManageDetails></ManageDetails>
+    </div>
+  );
+};
+
+export default page;
+// eaaaaa
