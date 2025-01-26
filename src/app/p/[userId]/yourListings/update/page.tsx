33d8b7e8746a1fdb375a576acc6347e30d33e799
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2025-01-26 20:29:06 +0545

    add ManageDetails component for listing management page

---
+++ src/app/p/[userId]/yourListings/update/page.tsx
@@ -0,0 +1,11 @@
+import ManageDetails from "@/components/ManageDetails";
+
+const page = () => {
+  return (
+    <div>
+      <ManageDetails></ManageDetails>
+    </div>
+  );
+};
+
+export default page;
