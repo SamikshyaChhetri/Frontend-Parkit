7a134753b9f211971c2a1182a0b682f9b72a8190
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2025-01-28 15:33:09 +0545

    issue #17 solved

--- src/app/p/[userId]/listings/[listingsId]/page.tsx
+++ src/app/p/[userId]/listings/[listingsId]/page.tsx
@@ -345,9 +345,15 @@ const Page: FC<{
               />
             </div>
             <div className="flex justify-end mt-3">
-              <button className="bg-violet-800 text-white px-6 py-2 rounded-lg hover:bg-violet-700 transition">
-                Submit
-              </button>
+              <Button
+                className=" text-white px-6 py-2 rounded-lg hover:bg-violet-700 transition"
+                disabled={submitReview.isPending}
+              >
+                Submit{" "}
+                {submitReview.isPending && (
+                  <Icon icon="svg-spinners:270-ring" width="24" height="24" />
+                )}
+              </Button>
             </div>
           </form>
         </div>
