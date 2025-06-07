b551a8b09f620e05d35b556be10375843ec09836
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2025-06-07 17:19:41 +0545

    feat: implement CountryDropdown component for country selection

--- src/app/p/[userId]/settings/page.tsx
+++ src/app/p/[userId]/settings/page.tsx
@@ -1,11 +1,28 @@
-"use client";
-import React, { FC } from "react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
-const Page: FC<{
-  params: Promise<{ userId: string }>;
-}> = ({ params }) => {
-  const resolvedParams = React.use(params);
-
-  return <div>{/* <Header></Header> */}</div>;
+const page = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <div>Profile</div>
+          <div>View and edit all your profile details here</div>
+        </div>
+        <div>
+          <div>Samiksya Baniya</div>
+          <div>samikshya@gmail.com</div>
+          <img src="/bike.jpg" alt="userImage" />
+        </div>
+        <div>
+          <div>
+            <Label htmlFor="email">Email</Label>
+            <Input type="email" id="email" placeholder="Email" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
-export default Page;
+
+export default page;
